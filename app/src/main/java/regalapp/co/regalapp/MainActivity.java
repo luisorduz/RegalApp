934577e793e64aa4764dbd137849a1b9c35603c0
luisorduz <andresorduz1@gmail.com>
2015-09-23 17:03:14 -0500

    Oncreate solo.

--- app/src/main/java/regalapp/co/regalapp/MainActivity.java
+++ app/src/main/java/regalapp/co/regalapp/MainActivity.java
@@ -3,7 +3,6 @@ package regalapp.co.regalapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +13,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
 }
