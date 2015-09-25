f52bee1494befaf0face80774fd2862ab3c6982b
luisorduz <andresorduz1@gmail.com>
2015-09-25 10:03:02 -0500

    packages

--- app/src/main/java/regalapp/co/regalapp/regaControlador/MainActivity.java
+++ app/src/main/java/regalapp/co/regalapp/regaControlador/MainActivity.java
@@ -1,7 +1,9 @@
-package regalapp.co.regalapp;
+package regalapp.co.regalapp.regaControlador;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import regalapp.co.regalapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
