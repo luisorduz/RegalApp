ab8a89856e7c964e8783ae9d5b83a7b4dbd911b8
luisorduz <andresorduz1@gmail.com>
2015-09-25 16:16:44 -0500

    actualizacion

--- app/src/main/java/regalapp/co/regalapp/regaControlador/MainActivity.java
+++ app/src/main/java/regalapp/co/regalapp/regaControlador/MainActivity.java
@@ -1,16 +1,22 @@
 package regalapp.co.regalapp.regaControlador;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import regalapp.co.regalapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
