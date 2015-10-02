1ade36baee73c11ed2efa11c78179edbbb8b7e54
luisorduz <andresorduz1@gmail.com>
2015-10-02 17:08:49 -0500

    Camara

--- app/src/main/java/regalapp/co/regalapp/regaControlador/PublicarRegaloControlador.java
+++ app/src/main/java/regalapp/co/regalapp/regaControlador/PublicarRegaloControlador.java
@@ -1,7 +1,17 @@
 package regalapp.co.regalapp.regaControlador;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import com.andexert.library.RippleView;
+
+import java.io.File;
+import java.util.Date;
 
 import regalapp.co.regalapp.R;
 
@@ -10,9 +20,26 @@ import regalapp.co.regalapp.R;
  */
 public class PublicarRegaloControlador  extends Activity{
 
+
+    private RippleView rippleView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publicar_regalo);
+
+        rippleView = (RippleView) findViewById(R.id.RPtomar_Foto);
+
+
+        rippleView.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+
+            @Override
+            public void onComplete(RippleView rippleView) {
+
+            }
+
+        });
     }
+
+
 }
