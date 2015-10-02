f14aff26b988512d6fe99353df5f22e02e06c3be
luisorduz <andresorduz1@gmail.com>
2015-10-02 16:12:30 -0500

    Camara

--- app/src/main/java/regalapp/co/regalapp/regaControlador/HomeControlador.java
+++ app/src/main/java/regalapp/co/regalapp/regaControlador/HomeControlador.java
@@ -1,8 +1,10 @@
 package regalapp.co.regalapp.regaControlador;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -13,15 +15,18 @@ import java.util.HashMap;
 
 import regalapp.co.regalapp.R;
 import regalapp.co.regalapp.regaAdaptadores.CategoriasArrayAdapter;
+import regalapp.co.regalapp.regaUtilidades.RippleView;
 
 /**
  * Created by AndresOs on 29/09/15.
  */
-public class CasaControlador extends Activity {
+public class HomeControlador extends Activity {
 
     private ListView LVcategorias;
     CategoriasArrayAdapter categoriasArrayAdapter;
     ArrayList<HashMap<String, String>> arrayCategoria = new ArrayList<HashMap<String, String>>();
+
+    private com.andexert.library.RippleView btn_publicar_regalo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +34,24 @@ public class CasaControlador extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_home);
 
+        btn_publicar_regalo = (com.andexert.library.RippleView) findViewById(R.id.publicaRegalo);
         LVcategorias = (ListView) findViewById(R.id.LV_categorias);
 
         //String.valueOf(R.drawable.ic_action_redeem
         String[] categorias = new  String[]{"Hogar","Electrodomésticos","Deportes","Mascotas",
                                             "Tecnología","Servicios","Uso hombre","Uso mujer"};
-
         categoriasArrayAdapter = new CategoriasArrayAdapter(this,categorias);
-
         LVcategorias.setAdapter(categoriasArrayAdapter);
+
+        btn_publicar_regalo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent publicar_regalo = new Intent(getApplicationContext(),PublicarRegaloControlador.class);
+                startActivity(publicar_regalo);
+            }
+        });
+
+
 
     }
 }
---
+++ app/src/main/java/regalapp/co/regalapp/regaControlador/PublicarRegaloControlador.java
@@ -0,0 +1,18 @@
+package regalapp.co.regalapp.regaControlador;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import regalapp.co.regalapp.R;
+
+/**
+ * Created by AndresOs on 2/10/15.
+ */
+public class PublicarRegaloControlador  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publicar_regalo);
+    }
+}
