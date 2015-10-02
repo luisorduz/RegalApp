5263f001ab6ec84d2828b282a08249f6adcbe17a
luisorduz <andresorduz1@gmail.com>
2015-10-02 00:12:53 -0500

    Cambios graficos ok

--- app/src/main/java/regalapp/co/regalapp/regaAdaptadores/CategoriasArrayAdapter.java
+++ app/src/main/java/regalapp/co/regalapp/regaAdaptadores/CategoriasArrayAdapter.java
@@ -1,6 +1,8 @@
 package regalapp.co.regalapp.regaAdaptadores;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,23 +26,46 @@ public class CategoriasArrayAdapter extends ArrayAdapter<String> {
         this.values = values;
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
         View rowView = inflater.inflate(R.layout.adapter_categorias, parent, false);
-        RippleView textView = (RippleView) rowView.findViewById(R.id.TVcategoria);
+        TextView textView = (TextView) rowView.findViewById(R.id.TVcategoria);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.IMcategoria);
         imageView.setBackground(null);
         textView.setText(values[position]);
         // Change the icon for Windows and iPhone
         String s = values[position];
-        if (s.startsWith("Cat1")) {
-            imageView.setImageResource(R.drawable.ic_social_person);
-        } else {
-            imageView.setImageResource(R.drawable.ic_action_redeem);
+
+        if (s.startsWith("Hogar")) {
+            imageView.setImageResource(R.drawable.ic_action_home);
         }
 
+        if (s.startsWith("Electrodomésticos")) {
+            imageView.setImageResource(R.drawable.ic_electrodomesticos);
+        }
+        if (s.startsWith("Deportes")) {
+            imageView.setImageResource(R.drawable.ic_deportes);
+        }
+        if (s.startsWith("Mascotas")) {
+            imageView.setImageResource(R.drawable.ic_mascotas);
+        }
+        if (s.startsWith("Tecnología")) {
+            imageView.setImageResource(R.drawable.ic_tecnologia);
+        }
+        if (s.startsWith("Servicios")) {
+            imageView.setImageResource(R.drawable.ic_servicios);
+        }
+        if (s.startsWith("Uso hombre")) {
+            imageView.setImageResource(R.drawable.ic_uso_hombre);
+        }
+        if (s.startsWith("Uso mujer")) {
+            imageView.setImageResource(R.drawable.ic_uso_mujer);
+        }
         return rowView;
     }
 }
